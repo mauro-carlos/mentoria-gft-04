4e205424e54a37c6bb473bd60b713a06cc95ee8e
Mauro Carlos <mauro_carlos14@hotmail.com>
2025-09-19 13:06:02 -0400

    commit do dirretorio implementado a logica do gerenciamento colaborador

--- src/me/dio/models/Gerente.java
+++ src/me/dio/models/Gerente.java
@@ -5,6 +5,7 @@ public class Gerente extends FuncionarioPJ implements CalculaBonificacao{
   private Double valorBonificacao;
 
 
+
   @Override
   public void calculaBonificacao(Double porcentagemBonificacao) {
     this.valorBonificacao = (super.getValorRemuneracao() * (porcentagemBonificacao/100)) + 100d;
